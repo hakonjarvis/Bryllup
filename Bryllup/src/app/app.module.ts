442d0d5a7261bafc68a63087e8e4e23b90319fe5
Håkon Jarvis Westergård <hakon.jarvis.westergard@afgruppen.no>
2021-02-21 20:27:09 +0100

    ..

---
+++ Bryllup/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './Components/navigation-bar/navigation-bar.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { ContentComponent } from './Components/content/content.component';
+import { CountDownComponent } from './Components/count-down/count-down.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationBarComponent,
+    HeaderComponent,
+    ContentComponent,
+    CountDownComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
